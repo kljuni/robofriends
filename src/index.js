d0f95e4d740e59f4d2ca183dd6852c66ea6465a4
Ivan Kljun <ivankljun@gmail.com>
2020-02-08 18:09:56 +0100

    Adding new files

--- src/index.js
+++ src/index.js
@@ -2,16 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import "tachyons";
-import CardList from "./CardList"
-import {robots} from "./robots";
+import App from "./App";
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-				console.log(robots[1])
-				<div>
-					<CardList/>
-				</div>
-
+ReactDOM.render(<App />
 
 	, document.getElementById('root'));
 
